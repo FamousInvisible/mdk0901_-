4f840bbcf5a5ac902d98217615c941b00481b3fb
FamousInvisible <sania.prihodko15@gmail.com>
2020-04-24 13:22:13 +0300

    Добавлено задание 1

---
+++ script1.js
@@ -0,0 +1,13 @@
+const spisok = document.querySelector("#spisok"),
+button = document.querySelector("button"),
+input = document.querySelector("input");
+
+button.addEventListener("click", add);
+
+function add(){
+    let create = document.createElement("li");
+    create.textContent = input.value;
+    input.value = "";
+    spisok.append(create);
+}
+console.log("Писос");
